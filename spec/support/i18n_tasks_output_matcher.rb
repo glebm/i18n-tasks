19fc64dcbefd1b07bdaaec837e2283c08b6c2fa0
gleb <glex.spb@gmail.com>
2013-06-29 14:33:15 +0200

    better spec helper

--- spec/support/i18n_tasks_output_matcher.rb
+++ spec/support/i18n_tasks_output_matcher.rb
@@ -1,14 +1,25 @@
 RSpec::Matchers.define :be_i18n_keys do |expected|
-  match do |actual|
-    r = actual.split("\n").map { |x|
+  LOC_PREFIX = /^\w{2}\b/
+
+  extract_keys = ->(actual){
+    locales = I18n.available_locales.map(&:to_s)
+    actual.split("\n").map { |x|
       x.strip!
       key = x.gsub(/\s+/, ' ').split(' ').reverse.detect { |p| p && p.include?('.') }
-      if x =~ /^\w{2}\b/
+      if x =~ LOC_PREFIX && locales.include?(x[0..1]) && !(key =~ LOC_PREFIX && locales.include(key[0..1]))
         x.split(' ', 2)[0] + '.' + key
       else
         key
       end
     }
-    r.sort == expected.sort
+  }
+
+  match do |actual|
+    extract_keys.(actual).sort == expected.sort
+  end
+
+  failure_message_for_should do |actual|
+    "Expected #{expected.sort}, but had #{extract_keys.(actual).sort}"
+
   end
 end
