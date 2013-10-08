fb9661d2b20c56904747aeb95b4941225f379d5f
Gleb Mazovetskiy <glex.spb@gmail.com>
2013-10-08 17:45:23 +0200

    improve formatting

--- lib/i18n/tasks/output/terminal.rb
+++ lib/i18n/tasks/output/terminal.rb
@@ -5,17 +5,17 @@ module I18n
       class Terminal
         include Term::ANSIColor
 
-        def missing(keys)
-          $stderr.puts bold cyan "Missing keys and translations (#{keys.length})"
-          $stderr.puts cyan <<-TEXT
-        Legend: #{red '✗'} key missing, #{yellow bold '∅'} translation blank, #{yellow bold '='} value equal to base locale.
-          TEXT
-          keys.each { |m| print_missing_translation m }
+        def missing(missing)
+          $stderr.puts bold cyan "Missing keys and translations (#{missing.length})"
+          $stderr.puts "#{bold 'Legend:'} #{red '✗'} key missing, #{yellow bold '∅'} translation blank, #{blue bold '='} value equal to base locale; #{cyan 'value in base locale'}"
+          key_col_width = missing.map { |x| x[:key] }.max_by(&:length).length + 2
+          missing.each { |m| print_missing_translation m, key_col_width: key_col_width }
         end
 
-        def unused(keys)
-          $stderr.puts bold cyan("Unused i18n keys (#{keys.length})")
-          keys.each { |(key, value)| puts " #{magenta(key).ljust(60)}\t#{cyan value}" }
+        def unused(unused)
+          $stderr.puts bold cyan("Unused i18n keys (#{unused.length})")
+          key_col_width = unused.max_by { |x| x[0].length }[0].length + 2
+          unused.each { |(key, value)| puts "#{magenta key.ljust(key_col_width)}#{cyan value.strip}" }
         end
 
         private
@@ -24,17 +24,17 @@ module I18n
         STATUS_TEXTS = {
             none:    red("✗".ljust(6)),
             blank:   yellow(bold '∅'.ljust(6)),
-            eq_base: yellow(bold "=".ljust(6))
+            eq_base: blue(bold "=".ljust(6))
         }
 
-        def print_missing_translation(m)
-          locale, key, base_value, status_text = m[:locale], m[:key], m[:base_value], " #{STATUS_TEXTS[m[:type]]}"
+        def print_missing_translation(m, opts)
+          locale, key, base_value, status_text = m[:locale], m[:key], m[:base_value].try(:strip), " #{STATUS_TEXTS[m[:type]]}"
 
-          key = magenta(key).ljust(50)
+          key = magenta key.ljust(opts[:key_col_width])
           s   = if m[:type] == :none
-                  "#{red bold(locale.ljust(5))} #{status_text} #{key}"
+                  "#{red bold locale.ljust(4)} #{status_text} #{key}"
                 else
-                  " #{bold(locale.ljust(5))} #{status_text} #{key} #{cyan base_value}"
+                  "#{bold locale.ljust(4)} #{status_text} #{key} #{cyan base_value}"
                 end
           puts s
         end
