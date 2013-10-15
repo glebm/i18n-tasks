c531ce7dde54d676cab8eb0186d35da424be50f0
Peter Lampesberger <lichtamberg@github.com>
2013-10-15 13:59:21 +0200

    Fixed yml number bug

--- lib/i18n/tasks/output/terminal.rb
+++ lib/i18n/tasks/output/terminal.rb
@@ -15,7 +15,7 @@ module I18n
         def unused(unused)
           $stderr.puts bold cyan("Unused i18n keys (#{unused.length})")
           key_col_width = unused.max_by { |x| x[0].length }[0].length + 2
-          unused.each { |(key, value)| puts "#{magenta key.ljust(key_col_width)}#{cyan value.strip}" }
+          unused.each { |(key, value)| puts "#{magenta key.ljust(key_col_width)}#{cyan value.to_s.strip}" }
         end
 
         private
