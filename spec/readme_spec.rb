4d57043df6f9c89ce3deae9b5b9cdbe3d6033a97
Gleb Mazovetskiy <glex.spb@gmail.com>
2013-11-10 02:22:28 +0100

    Update readme_spec.rb

--- spec/readme_spec.rb
+++ spec/readme_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 describe 'README.md' do
   let(:readme) { File.read('README.md') }
-  it '```yaml blocks' do
+  it 'has YAML hashes in ```yaml blocks' do
     readme.scan /```yaml\n(.*)(?=^)\n```/ do |m|
       YAML.load(m[0]).should be_a(Hash)
     end
