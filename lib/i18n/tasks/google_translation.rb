5044338e76bda236bb7d7240f0852b6d550b43f5
Gleb Mazovetskiy <glex.spb@gmail.com>
2013-11-25 17:11:21 +0100

    Google translate API key helpful error message

--- lib/i18n/tasks/google_translation.rb
+++ lib/i18n/tasks/google_translation.rb
@@ -7,6 +7,10 @@ module I18n::Tasks::GoogleTranslation
     if (key = translation_config[:api_key]).present?
       opts[:key] ||= key
     end
+    if opts[:key].blank?
+      $stderr.puts(Term::ANSIColor.red Term::ANSIColor.yellow 'You may need to provide Google API key as GOOGLE_TRANSLATE_API_KEY or in config/i18n-tasks.yml.
+You can obtain the key at https://code.google.com/apis/console.')
+    end
     EasyTranslate.translate strings, opts
   end
 end
