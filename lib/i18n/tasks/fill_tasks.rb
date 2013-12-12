2629238da3d96a45d719b24a4e49af2eec748ead
Gleb Mazovetskiy <glex.spb@gmail.com>
2013-12-12 16:24:26 +0100

    refs #29 - only translate string keys

--- lib/i18n/tasks/fill_tasks.rb
+++ lib/i18n/tasks/fill_tasks.rb
@@ -1,7 +1,7 @@
 module I18n::Tasks::FillTasks
   def add_missing!(locale = base_locale, placeholder = nil)
     normalize_store! locale
-    set_blank_values! locale, locale == base_locale do |keys|
+    set_blank_values! locale do |keys|
       keys.map { |key|
         placeholder || key.split('.').last.to_s.humanize
       }
@@ -22,8 +22,13 @@ module I18n::Tasks::FillTasks
     locales = non_base_locales(locales)
     normalize_store! locales
     locales.each do |locale|
-      set_blank_values! locale do |blank_keys|
-        google_translate blank_keys.map { |k| t(k) }, to: locale, from: base_locale
+      blank_keys = find_blank_keys(locale).select { |k|
+        tr = t(k)
+        tr.present? && tr.is_a?(String)
+      }
+      if blank_keys.present?
+        translated   = blank_keys.zip google_translate(blank_keys.map { |k| t(k) }, to: locale, from: base_locale)
+        data[locale] = data[locale].deep_merge(list_to_tree translated)
       end
     end
   end
@@ -41,14 +46,18 @@ module I18n::Tasks::FillTasks
 
   # fill blank values with values from passed block
   # @param [String] locale
-  # @param [true, false] include_missing Include keys present in source but missing from base locale
-  def set_blank_values!(locale = base_locale, include_missing = (locale == base_locale), &fill_with)
+  def set_blank_values!(locale = base_locale, &fill_with)
+    blank_keys   = find_blank_keys locale
+    list         = blank_keys.zip fill_with.call(blank_keys)
+    data[locale] = data[locale].deep_merge(list_to_tree(list))
+  end
+
+
+  def find_blank_keys(locale, include_missing = (locale == base_locale))
     blank_keys = traverse_map_if(data[base_locale]) { |key, value|
       key if !key_value?(key, locale) && !ignore_key?(key, :missing)
     }
     blank_keys += keys_not_in_base if include_missing
-
-    list         = blank_keys.uniq.zip fill_with.call(blank_keys)
-    data[locale] = data[locale].deep_merge(list_to_tree(list))
+    blank_keys.uniq
   end
 end
