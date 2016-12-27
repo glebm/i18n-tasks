30525288384efb7e7d6e235712a887cbee10aa2f
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-12-27 01:36:56 +0000

    spec/bin_simplecov_helper: neater command name

--- spec/bin_simplecov_helper.rb
+++ spec/bin_simplecov_helper.rb
@@ -2,7 +2,7 @@
 if ENV['COVERAGE'] && !%w(rbx jruby).include?(RUBY_ENGINE)
   ENV['SIMPLECOV_NO_DEFAULTS'] = '1'
   require 'simplecov'
-  SimpleCov.command_name "#{$PROGRAM_NAME} #{ARGV.join ' '}".strip
+  SimpleCov.command_name "#{$PROGRAM_NAME.sub(%r{^([.]{2}/)+}, '')} #{ARGV.join ' '}".strip
   SimpleCov.root File.expand_path(File.join(File.dirname(__FILE__), '..'))
   require 'simplecov/defaults'
   SimpleCov::Formatter::HTMLFormatter.send(:define_method, :puts) { |*| }
