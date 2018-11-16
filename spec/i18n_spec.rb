8e203c403abb600e7711744addfdb4bccfed8025
Nikita Vasilevsky <nvasilevskii@gmail.com>
2018-11-16 17:55:27 +0300

    Add inconsistent interpolations check to test templates

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -6,15 +6,16 @@ RSpec.describe 'I18n' do
   let(:i18n) { I18n::Tasks::BaseTask.new }
   let(:missing_keys) { i18n.missing_keys }
   let(:unused_keys) { i18n.unused_keys }
+  let(:inconsistent_interpolations) { i18n.inconsistent_interpolations }
 
   it 'does not have missing keys' do
     expect(missing_keys).to be_empty,
-                            "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
+                            "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing` to show them"
   end
 
   it 'does not have unused keys' do
     expect(unused_keys).to be_empty,
-                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
+                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused` to show them"
   end
 
   it 'files are normalized' do
@@ -28,5 +29,11 @@ RSpec.describe 'I18n' do
                     "#{non_normalized.map { |path| "  #{path}" }.join("\n")}\n" \
                     'Please run `i18n-tasks normalize` to fix'
     expect(non_normalized).to be_empty, error_message
+  end
+
+  it 'does not have inconsistent interpolations' do
+    error_message = "#{inconsistent_interpolations.leaves.count} i18n keys have inconsistent interpolations.\n" \
+                    'Please run `i18n-tasks check-consistent-interpolations` to show them'
+    expect(inconsistent_interpolations).to be_empty, error_message
   end
 end
--- templates/minitest/i18n_test.rb
+++ templates/minitest/i18n_test.rb
@@ -11,12 +11,12 @@ class I18nTest < ActiveSupport::TestCase
 
   def test_no_missing_keys
     assert_empty @missing_keys,
-                 "Missing #{@missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
+                 "Missing #{@missing_keys.leaves.count} i18n keys, run `i18n-tasks missing` to show them"
   end
 
   def test_no_unused_keys
     assert_empty @unused_keys,
-                 "#{@unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
+                 "#{@unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused` to show them"
   end
 
   def test_files_are_normalized
@@ -25,5 +25,12 @@ class I18nTest < ActiveSupport::TestCase
                     "#{non_normalized.map { |path| "  #{path}" }.join("\n")}\n" \
                     'Please run `i18n-tasks normalize` to fix'
     assert_empty non_normalized, error_message
+  end
+
+  def test_no_inconsistent_interpolations
+    inconsistent_interpolations = @i18n.inconsistent_interpolations
+    error_message = "#{inconsistent_interpolations.leaves.count} i18n keys have inconsistent interpolations.\n" \
+                    'Please run `i18n-tasks check-consistent-interpolations` to show them'
+    assert_empty inconsistent_interpolations, error_message
   end
 end
