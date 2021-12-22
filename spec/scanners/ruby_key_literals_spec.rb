6b496fa421b0e870053a1da1fabd64616b51dc11
otoyo <hiroki.t.1988@gmail.com>
2021-12-23 08:29:33 +0900

    Revert "Add specs for Ruby literal key regexp"
    This reverts commit 06279731196adf43c37a99b9342f96158a316af8.

--- spec/scanners/ruby_key_literals_spec.rb
+++ spec/scanners/ruby_key_literals_spec.rb
@@ -8,42 +8,6 @@ RSpec.describe 'RubyKeyLiterals' do
     Object.new.extend I18n::Tasks::Scanners::RubyKeyLiterals
   end
 
-  describe '#literal_re' do
-    subject do
-      /(#{scanner.literal_re})/x =~ key
-      Regexp.last_match(1)
-    end
-
-    context 'string' do
-      context 'single quoted' do
-        let(:key) { %('some_key') }
-        it { is_expected.to eq(key) }
-      end
-
-      context 'double quoted' do
-        let(:key) { %q("#{some_key}") } # rubocop:disable Lint/InterpolationCheck
-        it { is_expected.to eq(key) }
-      end
-    end
-
-    context 'symbol' do
-      context 'regular literal' do
-        let(:key) { %(:some_key) }
-        it { is_expected.to eq(key) }
-      end
-
-      context 'single quoted' do
-        let(:key) { %(:'some_key') }
-        it { is_expected.to eq(key) }
-      end
-
-      context 'double quoted' do
-        let(:key) { %q(:"#{some_key}") } # rubocop:disable Lint/InterpolationCheck
-        it { is_expected.to eq(key) }
-      end
-    end
-  end
-
   describe '#valid_key?' do
     it 'allows forward slash in key' do
       expect(scanner).to be_valid_key('category/product')
