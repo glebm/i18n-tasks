0c7b9c77dab548b539626e734fada32c6e7780d5
Liberatys <nick.flueckiger@renuo.ch>
2022-03-22 15:13:43 +0100

    Repair emojis after dump of yaml

---
+++ spec/emoji_retention_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Emoji Retention in dump' do
+  let!(:task) { I18n::Tasks::BaseTask.new }
+
+  let(:yaml) { { 'a' => 'hello %{world}😀', 'b' => 'foo', 'c' => { 'd' => 'hello %{name}' }, 'e' => 'ok' } }
+
+  describe '.dump' do
+    it 'does not strip emojis from yaml' do
+      dumped_yaml = I18n::Tasks::Data::Adapter::YamlAdapter.dump(
+        yaml,
+        {}
+      )
+      expect(dumped_yaml).to include('😀')
+    end
+  end
+end
