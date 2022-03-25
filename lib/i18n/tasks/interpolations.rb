6a2e9958f6f4b17528460cfd050bcd6528f1f9f9
Gleb Mazovetskiy <glex.spb@gmail.com>
2022-03-25 09:06:44 +0000

    Make interpolations variable regex configurable
    For better `it` gem support. Refs #361

--- lib/i18n/tasks/interpolations.rb
+++ lib/i18n/tasks/interpolations.rb
@@ -2,22 +2,26 @@
 
 module I18n::Tasks
   module Interpolations
-    VARIABLE_REGEX = /%{[^}]+}/.freeze
+    class << self
+      attr_accessor :variable_regex
+    end
+    @variable_regex = /%{[^}]+}/.freeze
 
     def inconsistent_interpolations(locales: nil, base_locale: nil) # rubocop:disable Metrics/AbcSize
       locales ||= self.locales
       base_locale ||= self.base_locale
       result = empty_forest
+      variable_regex = I18n::Tasks::Interpolations.variable_regex
 
       data[base_locale].key_values.each do |key, value|
         next if !value.is_a?(String) || ignore_key?(key, :inconsistent_interpolations)
 
-        base_vars = Set.new(value.scan(VARIABLE_REGEX))
+        base_vars = Set.new(value.scan(variable_regex))
         (locales - [base_locale]).each do |current_locale|
           node = data[current_locale].first.children[key]
           next unless node&.value.is_a?(String)
 
-          if base_vars != Set.new(node.value.scan(VARIABLE_REGEX))
+          if base_vars != Set.new(node.value.scan(variable_regex))
             result.merge!(node.walk_to_root.reduce(nil) { |c, p| [p.derive(children: c)] })
           end
         end
