5e796eeacdfbeb53ddd802e21ffbf13cac8803e9
mario <github@lxxxvi.ch>
2022-10-25 17:20:49 +0200

    move instance variable assignment out of setup in minitest template

--- templates/minitest/i18n_test.rb
+++ templates/minitest/i18n_test.rb
@@ -5,18 +5,18 @@ require 'i18n/tasks'
 class I18nTest < ActiveSupport::TestCase
   def setup
     @i18n = I18n::Tasks::BaseTask.new
-    @missing_keys = @i18n.missing_keys
-    @unused_keys = @i18n.unused_keys
   end
 
   def test_no_missing_keys
-    assert_empty @missing_keys,
-                 "Missing #{@missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
+    missing_keys = @i18n.missing_keys
+    assert_empty missing_keys,
+                 "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
   end
 
   def test_no_unused_keys
-    assert_empty @unused_keys,
-                 "#{@unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
+    unused_keys = @i18n.unused_keys
+    assert_empty unused_keys,
+                 "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
   end
 
   def test_files_are_normalized
