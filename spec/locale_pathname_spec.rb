272ccd1e4c7d459016d59846664db3ee5c699300
Nick Schonning <nschonni@gmail.com>
2023-04-19 21:25:02 -0400

    chore: RSpec/ContextMethod

--- spec/locale_pathname_spec.rb
+++ spec/locale_pathname_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe 'LocalePathname' do
-  context '#replace_locale' do
+  describe '#replace_locale' do
     def replace_locale(path, from, to)
       I18n::Tasks::LocalePathname.replace_locale(path, from, to)
     end
