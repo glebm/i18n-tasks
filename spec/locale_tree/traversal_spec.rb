a1a31ac21f088a37cd217120886ff39254293729
Gleb Mazovetskiy <glex.spb@gmail.com>
2023-07-10 04:05:12 +0100

    Fix rubocop

--- spec/locale_tree/traversal_spec.rb
+++ spec/locale_tree/traversal_spec.rb
@@ -1,20 +1,21 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-RSpec.describe I18n::Tasks::Data::Tree::Traversal do
+RSpec.describe I18n::Tasks::Data::Tree::Traversal do # rubocop:disable RSpec/FilePath
   delegate :i18n_task, to: :TestCodebase
 
-  describe '#grep_keys' do
-    it 'returns Siblings' do
-      expect(i18n_task.data['en'].grep_keys(/key/)).to be_a(I18n::Tasks::Data::Tree::Siblings)
-    end
-  end
-
-  # --- setup ---
-  before(:each) do
+  before do
     TestCodebase.setup fixtures_contents
   end
 
   after do
     TestCodebase.teardown
+  end
+
+  describe '#grep_keys' do
+    it 'returns Siblings' do
+      expect(i18n_task.data['en'].grep_keys(/key/)).to be_a(I18n::Tasks::Data::Tree::Siblings)
+    end
   end
 end
