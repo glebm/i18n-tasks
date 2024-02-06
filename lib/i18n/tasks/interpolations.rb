e7991864ea745aa2ef03a614b0e140d9bab79bc9
Tom Brouwer <tom.brouwer@bookingexperts.nl>
2024-02-06 16:37:58 +0100

    20321 Fix liquid tag consistency not being validated in interpolations check
    Note that interpolation checks can be skipped on specific keys using
    the 'ignore_inconsistent_interpolations' property in the yaml config
    file
    
    Ref bookingexperts/support#20321

--- lib/i18n/tasks/interpolations.rb
+++ lib/i18n/tasks/interpolations.rb
@@ -3,25 +3,30 @@
 module I18n::Tasks
   module Interpolations
     class << self
-      attr_accessor :variable_regex
+      attr_accessor :variable_regex, :tag_pairs, :tag_with_localized_value_regex
     end
-    @variable_regex = /(?<!%)%{[^}]+}/.freeze
+    @variable_regex = /(?<!%)%\{[^}]+\}|\{\{.*?\}\}|\{%.*?%\}/.freeze
+    @tag_pairs = [
+      ['{{', '}}'],
+      ['%{', '}'],
+      ['{%', '%}']
+    ].freeze
+    @tag_with_localized_value_regex = /\{\{\s?(("[^"]+")|('[^']+'))\s?\|.*?\}\}/
 
     def inconsistent_interpolations(locales: nil, base_locale: nil) # rubocop:disable Metrics/AbcSize
       locales ||= self.locales
       base_locale ||= self.base_locale
       result = empty_forest
-      variable_regex = I18n::Tasks::Interpolations.variable_regex
 
       data[base_locale].key_values.each do |key, value|
         next if !value.is_a?(String) || ignore_key?(key, :inconsistent_interpolations)
 
-        base_vars = Set.new(value.scan(variable_regex))
+        base_vars = get_normalized_variables_set(value)
         (locales - [base_locale]).each do |current_locale|
           node = data[current_locale].first.children[key]
           next unless node&.value.is_a?(String)
 
-          if base_vars != Set.new(node.value.scan(variable_regex))
+          if base_vars != get_normalized_variables_set(node.value)
             result.merge!(node.walk_to_root.reduce(nil) { |c, p| [p.derive(children: c)] })
           end
         end
@@ -29,6 +34,28 @@ module I18n::Tasks
 
       result.each { |root| root.data[:type] = :inconsistent_interpolations }
       result
+    end
+
+    def get_normalized_variables_set(string)
+      Set.new(string.scan(I18n::Tasks::Interpolations.variable_regex).map { |variable| normalize(variable) })
+    end
+
+    def normalize(variable)
+      normalized = nil
+      if (match = variable.match(I18n::Tasks::Interpolations.tag_with_localized_value_regex))
+        variable = variable.sub(match[1], 'localized input')
+      end
+      I18n::Tasks::Interpolations.tag_pairs.each do |start, end_|
+        next unless variable.start_with?(start)
+
+        normalized = variable.delete_prefix(start).delete_suffix(end_).strip
+        normalized = start + normalized + end_
+        break
+      end
+
+      fail 'No start/end tag pair detected' if normalized.nil?
+
+      normalized
     end
   end
 end
--- spec/interpolations_spec.rb
+++ spec/interpolations_spec.rb
@@ -25,13 +25,63 @@ RSpec.describe 'Interpolations' do
     TestCodebase.teardown
   end
 
-  it '#inconsistent_interpolation' do
-    wrong  = task.inconsistent_interpolations
-    leaves = wrong.leaves.to_a
+  context 'ruby string interpolations' do
+    let(:base_keys) { { 'a' => 'hello %{world}', 'b' => 'foo', 'c' => { 'd' => 'hello %{name}' }, 'e' => 'ok' } }
+    let(:test_keys) { { 'a' => 'hello', 'b' => 'foo %{bar}', 'c' => { 'd' => 'hola %{amigo}' }, 'e' => 'ok' } }
 
-    expect(leaves.size).to eq 3
-    expect(leaves[0].full_key).to eq 'es.a'
-    expect(leaves[1].full_key).to eq 'es.b'
-    expect(leaves[2].full_key).to eq 'es.c.d'
+    it 'detects inconsistent interpolations' do
+      wrong  = task.inconsistent_interpolations
+      leaves = wrong.leaves.to_a
+
+      expect(leaves.size).to eq 3
+      expect(leaves[0].full_key).to eq 'es.a'
+      expect(leaves[1].full_key).to eq 'es.b'
+      expect(leaves[2].full_key).to eq 'es.c.d'
+    end
+  end
+
+  context 'liquid tags' do
+    let(:base_keys) do
+      {
+        a: 'hello {{ world }}',
+        b: 'foo',
+        c: {
+          d: 'hello {{ name }}'
+        },
+        e: 'ok',
+        f: 'inconsistent {{ whitespace}}',
+        g: 'includes a {% comment %}',
+        h: 'wrong {% comment %}',
+        i: 'with localized value: {{ "thanks" | owner_invoice }}',
+        j: 'with wrong function: {{ "thanks" | owner_invoices }}'
+      }
+    end
+    let(:test_keys) do
+      {
+        a: 'hello',
+        b: 'foo {{ bar }}',
+        c: {
+          d: 'hola {{ amigo }}'
+        },
+        e: 'ok',
+        f: '{{whitespace }} inconsistentes',
+        g: 'incluye un {% comment %}',
+        h: '{% commentario %} equivocado',
+        i: 'con valor localizado: {{ "gracias" | owner_invoice }}',
+        j: 'con función incorrecta: {{ "thanks" | owner_invoice }}'
+      }
+    end
+
+    it 'detects inconsistent interpolations' do
+      wrong  = task.inconsistent_interpolations
+      leaves = wrong.leaves.to_a
+
+      expect(leaves.size).to eq 5
+      expect(leaves[0].full_key).to eq 'es.a'
+      expect(leaves[1].full_key).to eq 'es.b'
+      expect(leaves[2].full_key).to eq 'es.c.d'
+      expect(leaves[3].full_key).to eq 'es.h'
+      expect(leaves[4].full_key).to eq 'es.j'
+    end
   end
 end
