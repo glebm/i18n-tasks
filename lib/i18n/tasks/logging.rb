67f237ccc5ba98851c8ab07c6305a1938bcd5e2c
David Wessman <david.wesmn@gmail.com>
2025-06-29 18:46:18 +0200

    Chore: Restores logging method (#648)
    - Did not work on JRuby, so we use $stderr.puts directly

--- lib/i18n/tasks/logging.rb
+++ lib/i18n/tasks/logging.rb
@@ -25,7 +25,8 @@ module I18n::Tasks::Logging
   def log_stderr(*args)
     # We don't want output from different threads to get intermixed.
     MUTEX.synchronize do
-      warn(*args)
+      # Use $stderr directly to avoid issues with JRuby and thread safety
+      $stderr.puts(*args) # rubocop:disable Style/StderrPuts
     end
   end
 
