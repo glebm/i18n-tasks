6d5391d22af9d7ade7f9e2c6e26ad7a4b7a76a2c
Benjamin Stiegel <benjamin.stiegel@knestel.de>
2026-02-17 10:40:06 +0100

    Add Uvida translator to translation backends

--- lib/i18n/tasks/translation.rb
+++ lib/i18n/tasks/translation.rb
@@ -5,6 +5,7 @@ require "i18n/tasks/translators/google_translator"
 require "i18n/tasks/translators/openai_translator"
 require "i18n/tasks/translators/watsonx_translator"
 require "i18n/tasks/translators/yandex_translator"
+require "i18n/tasks/translators/uvida_translator"
 
 module I18n::Tasks
   module Translation
@@ -24,6 +25,8 @@ module I18n::Tasks
         Translators::WatsonxTranslator.new(self).translate_forest(forest, from)
       when :yandex
         Translators::YandexTranslator.new(self).translate_forest(forest, from)
+      when :uvida
+        Translators::UvidaTranslator.new(self).translate_forest(forest, from)
       else
         fail CommandError, "invalid backend: #{backend}"
       end
